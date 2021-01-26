4962864f10d429fcefd7ff990185d82518a5ac44
Jordi Colomer <60452813+jordicf10@users.noreply.github.com>
2021-01-26 20:36:18 +0100

    1 to 100
    A simply an effective code that prints every number between one and one hundred

---
+++ 1to100/src/one2onehundred.java
@@ -0,0 +1,17 @@
+
+public class one2onehundred {
+
+	public static void main(String[] args) {
+		/*for(int i=1;i<=100;i++) {
+			System.out.println(i);*/
+		
+		int x= 1;
+		
+		while(x<=100) {
+			System.out.println(x);
+			x++;
+		}
+
+	}
+
+}
